8e39e813fddeb5710cd56f0eeb24053065726e86
antoni <antoni.dev0@gmail.com>
2022-01-12 09:32:36 -0800

    adds basic todo component

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -4,9 +4,96 @@ import { v4 as uuidv4 } from 'uuid';
 
 const Todo = () => {
   
+  const [list,setList] = useState([])
+  const [incomplete, setIncomplete] = useState([])
+  const { handleChange, handleSubmit } = useForm(addItem)
+  
+  function addItem(item){
+    console.log('**addItem')
+    item.id = uuidv4()
+    item.complete = false
+    console.log('...list',[...list])
+    setList([...list,item])
+    console.log('[...list,item]',[...list,item])
+  }
+
+  function deleteItem(id){
+    const items = list.filter( item => item.id !== id )
+    setList(items)
+  }
+
+  function toggleComplete(id){
+
+    const items = list.map( item => {
+      if( item.id === id )item.complete = !item.complete
+      return item
+    } )
+    setList(items)
+  }
+
+  useEffect(()=>{
+    let incompleteCount = list.filter(item =>!item.complete).length
+    setIncomplete(incompleteCount)
+    document.title = `Todo List: ${incomplete} `
+  }, [list,incomplete])
+
   return (
     <>
-      Todo Component
+      <header>
+        <h1>
+          Todo List
+        </h1>
+        <h2>
+          {incomplete} items pending
+        </h2>
+      </header>
+      <form onSubmit={handleSubmit}>
+        <h2>Add Todo Item</h2>
+        
+        <label>
+          <span>Todo Item</span>
+          <input 
+            onChange={handleChange}
+            name="text"
+            type="text"
+            placeholder="Item Details"
+          />
+        </label>
+        <label>
+          <span>Assigned To</span>
+          <input 
+            onChange={handleChange}
+            name="assignee"
+            type="text"
+            placeholder="Assignee Name" 
+          />
+        </label>
+        <label>
+        <span>Difficulty</span>
+          <input 
+            onChange={handleChange}
+            name="difficulty"
+            defaultValue="3"
+            type="range"
+            min="1"
+            max="5"
+          />
+        </label>
+        <label>
+          <button type="submit">Add Item</button>
+        </label>
+      </form>
+      {list.map(item => (
+        <div key={item.id}>
+          <p>{item.text}</p>
+          <p><smal>Assigned to: {item.assignee}</smal>Difficulty: {item.difficulty}</p>
+          <p></p>
+          <div onClick={()=>toggleComplete(item.id)}>
+            Complete: {item.complete.toString()}
+          </div>
+          <hr />
+        </div>
+      ))}
     </>
   )
 }
--- src/hooks/form.jsx
+++ src/hooks/form.jsx
@@ -1,5 +1,24 @@
 import { useState } from 'react';
 
-const useForm = (cb) => {}
+const useForm = (cb) => {
+  const [values, setValues] =useState({})
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    cb(values)
+  }
+
+  const handleChange = (e) => {
+    e.persist()
+    console.log(`...${values}, [${e.target.name}]: ${e.target.value} `)
+    console.log('values', ...values)
+    setValues(
+      
+      values => ({...values, [e.target.name]: e.target.value})
+    )
+  }
+
+  return {handleChange, handleSubmit, values}
+}
 
 export default useForm;
