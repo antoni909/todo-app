220444296c6680c32fd7d2017802f2fc3addac53
antoni <antoni.dev0@gmail.com>
2022-01-16 20:37:37 -0800

    adds useStyle hook from MUI to useStyles.js file in hooks dir

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,5 +1,6 @@
 import { Link, Outlet } from 'react-router-dom';
-import { makeStyles } from '@material-ui/core'
+// import { makeStyles } from '@material-ui/core'
+import useStyles from '../hooks/useStyles'
 import Drawer from '@mui/material/Drawer';
 import Typography from '@material-ui/core/Typography'
 
@@ -10,44 +11,6 @@ import ListItemText from '@material-ui/core/ListItemText'
 import { HomeRounded, FormatListNumbered, ErrorOutline } from '@mui/icons-material';
 import SettingsIcon from '@mui/icons-material/Settings';
 
-const drawerWidth = 260
-// function that returns an obj using theme arg
-const useStyles = makeStyles((theme)=> {
-  return {
-    active: {
-      backgroundColor: '#f9f9f9',
-    },
-    button: {
-      fontSize: 15,
-      borderRadius: 10,
-      boxShadow: '5px 5px 2.5px 2px rgba(30, 203, 243, .3)',
-      backgroundColor: 'white',
-      '&:hover':{
-        backgroundColor: '#f11f11f11'
-      },
-      marginTop: '10px', 
-    },
-    drawer: {
-      width: drawerWidth
-    },
-    drawerPaper: {
-      width: drawerWidth
-    },
-    page: {
-      background: '#f11f11f11',
-      width: '100%',
-      borderRadius: 10,
-      boxShadow: '5px 5px 2.5px 2px rgba(30, 203, 243, .3)',
-    },
-    root : {
-      display: 'flex',
-    },
-    title: {
-      padding: theme.spacing(3)
-    },
-    toolbar: theme.mixins.toolbar,
-  }
-})
 
 const Layout = ({children}) => {
 
--- src/components/Settings.jsx
+++ src/components/Settings.jsx
@@ -2,6 +2,10 @@ import { useState,useEffect } from 'react'
 
 import Button from '@mui/material/Button';
 import Switch from '@mui/material/Switch';
+import Card from '@mui/material/Card';
+import CardHeader from '@mui/material/CardHeader';
+import CardContent from '@mui/material/CardContent';
+import Container from '@mui/material/Container';
 
 const Settings = () => {
 
@@ -19,23 +23,34 @@ const Settings = () => {
   },[defaults])
 
   return(
-    <div>
-      <h1>Update Your Settings:</h1>
-      <div>
-      <h2>Show Completed Todos</h2>
-      dont show<Switch onChange={()=> setDefaults({...defaults,completedTodos: !defaults.completedTodos}) } value={true} variant="contained"/>show
-      </div> 
-      <div>
-        number of todos per page
-        <Button
-          onClick={() => setDefaults({...defaults,totalTodos: defaults.totalTodos - 1})}
-        > - </Button>
-        {defaults.totalTodos}
-        <Button
-          onClick={() => setDefaults({...defaults,totalTodos: defaults.totalTodos + 1})}
-        > + </Button>
-      </div>
-    </div>
+      <Container 
+          maxWidth="md"
+          align="center"
+
+        >
+          <h1>Adjust Settings</h1>
+          <Card boolean="true">
+            <CardHeader title='Show Completed'></CardHeader>
+            <CardContent>
+              dont show
+              <Switch 
+                onChange={()=> setDefaults({...defaults,completedTodos: !defaults.completedTodos})} 
+                value={true} variant="contained"
+                />show
+            </CardContent>
+          </Card>
+          <Card boolean="true">
+            <CardHeader title='Items per page'></CardHeader>
+              <Button
+                // className={classes.button}
+                onClick={() => setDefaults({...defaults,totalTodos: defaults.totalTodos - 1})}
+              > - </Button>
+              {defaults.totalTodos}
+              <Button
+                onClick={() => setDefaults({...defaults,totalTodos: defaults.totalTodos + 1})}
+              > + </Button>
+          </Card>
+      </Container>
   )
 }
 export default Settings;
